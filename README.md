2055ad55a5926e5766da89e0fc2e1f77d34f0599
Tomas Baca <klaxalk@gmail.com>
2018-11-07 22:20:35 +0100

    minor changes

--- README.md
+++ README.md
@@ -2,9 +2,23 @@
 
 A ROS node for basic morphologic track classification.
 
-TODO: To Be Done
+TODO: Readme under construction
 
 ![rospix](misc/rospix.png)
+
+## Dependencies
+
+To download the repository install *git* with *lfs*:
+```bash
+sudo apt install git git-lfs
+```
+
+To run the Python code, following python packages are required:
+* scikit-image
+* sklearn 0.20.0
+```
+sudo pip3 install scikit-image sklearn
+```
 
 ## References
 M. Jilek, _Processing of Radiation Data from the Timepix Sensor on the VZLUSAT-1 Satellite_,  Master's thesis, Czech Technical University in Prague, Faculty of Electrical Engineering, 2018.
--- scripts/classification.py
+++ scripts/classification.py
@@ -1,63 +1,50 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 import rospy
 import tf
 import csv
 import sys
-
 import numpy
+import warnings
 
+# from dataManager.DataContainer import BitmapContainer_Image, BitmapContainer_Segment
+from dataManager.DataManager import POSSIBLE_LABELS, FEATURE_NAMES, NUMBERS_LABELS_MAP
+
+# feature classification
 from dataProcessing.FeatureExtractor import FeatureExtractor
 from dataProcessing.ImagePreprocessor import ImagePreprocessor
 
-from rospix.msg import Image as RospixImage
-from dataManager.DataContainer import BitmapContainer_Image, BitmapContainer_Segment
-from dataManager.DataManager import POSSIBLE_LABELS, FEATURE_NAMES, NUMBERS_LABELS_MAP
-
+# classifier
 from sklearn.externals import joblib
 from sklearn.pipeline import Pipeline
+
+# ROS messages for images
+from sensor_msgs.msg import Image
+from rospix.msg import Image as RospixImage
+
+# OpenCV + ROS cv_bridgfe
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
-
-from sensor_msgs.msg import Image
-
-from ppretty import ppretty
-
-NUMBERS_LABELS_MAP = {-1:'none',
-                      1:'dot',
-                      2:'blob_small',
-                      3:'blob_big',
-                      4:'blob_branched',
-                      5:'track_straight',
-                      6:'track_curly',
-                      7:'drop',
-                      8:'other',
-                      9:'track_lowres'}
 
 class Classification:
 
     def imageCallback(self, data):
 
+        # compy the image data to numpy matrix
         np_image = numpy.zeros((256, 256), numpy.uint16)
-
         for i,pixel in enumerate(data.image):
-
             x = i // 256
             y = i % 256
-
             np_image[x, y] = pixel
-
         np_image = np_image.astype(numpy.uint16)
 
         # segment the image
         segments = self.image_preprocessor.segmentate_image(np_image)
 
-        if len(segments) == 0:
-            return
-
         # prepare the numpy matrix for the (segments x features)
         np_features = numpy.zeros((len(segments), len(FEATURE_NAMES)))
 
+        # for all localized segments
         for i,segment in enumerate(segments):
 
             # extract the features from the image
@@ -71,11 +58,14 @@ class Classification:
 
                 np_features[i, j] = features[feature_name]
 
+        # classify the segments into classes
         y_unknown = self.pipeline.predict(np_features)
 
+        # # print the segment lables
         # for i, track_type in enumerate(y_unknown):
         #     print("segment: {}, class: {}".format(i, NUMBERS_LABELS_MAP[track_type]))
 
+        # equalize the image histogram
         hist,bins = numpy.histogram(np_image.flatten(),65536,[0,65535])
         cdf = hist.cumsum()
         cdf_normalized = cdf * hist.max()/ cdf.max()
@@ -84,16 +74,20 @@ class Classification:
         cdf = numpy.ma.filled(cdf_m,0).astype('uint16')
         img2 = cdf[np_image]
 
+        # convert the image to 8bit range
         img2 = img2.astype(numpy.float)
         img_8bit = 255 - 255*img2/numpy.max(img2)
         img_8bit = img_8bit.astype(numpy.uint16)
         img_8bit = img_8bit.astype(numpy.uint8)
 
-        scale_factor = 8
-        upscaled = cv2.resize(img_8bit, dsize=(256*scale_factor, 256*scale_factor), interpolation = cv2.INTER_AREA)
+        # upscale the image to higher resolution
+        upscale_factor = 8
+        upscaled = cv2.resize(img_8bit, dsize=(256*upscale_factor, 256*upscale_factor), interpolation = cv2.INTER_AREA)
 
+        # convert the image to colored format
         upscaled = cv2.cvtColor(upscaled, cv2.COLOR_GRAY2BGR)
 
+        # highlight the segments
         for i, segment in enumerate(segments):
 
             corners = segment.get_metadata(key="corners")
@@ -107,10 +101,12 @@ class Classification:
             else:
                 color = (255, 255, 255)
 
-            cv2.rectangle(upscaled, (scale_factor*(corners[0][1]),scale_factor*(corners[0][0])+1), (scale_factor*(corners[2][1]+1),scale_factor*(corners[2][0]+1)), color, 3)
+            cv2.rectangle(upscaled, (upscale_factor*(corners[0][1]),upscale_factor*(corners[0][0])+1), (upscale_factor*(corners[2][1]+1),upscale_factor*(corners[2][0]+1)), color, 3)
 
+        # convert the image to ROS image
         image_message = self.bridge.cv2_to_imgmsg(upscaled, encoding="rgb8")
 
+        # publish the image
         self.publisher_image.publish(image_message)
 
         # proba = -1*numpy.ones((y_unknown.shape[0], len(POSSIBLE_LABELS[1:])))
@@ -120,30 +116,27 @@ class Classification:
 
         rospy.init_node('rospix_classification', anonymous=True)
 
+        # initialize the ROS cv_bridge
         self.bridge = CvBridge()
-
-        self.image = []
 
         # parameters
         self.pipeline_file = rospy.get_param('~pipeline_path', '/')
 
-        self.pipeline = joblib.load(self.pipeline_file) 
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=UserWarning)
+            self.pipeline = joblib.load(self.pipeline_file) 
 
+        # initialize the feature extraction
         self.feature_extractor = FeatureExtractor()
         self.image_preprocessor = ImagePreprocessor()
 
         # subscribers
         rospy.Subscriber("~image_in", RospixImage, self.imageCallback)
 
-        self.publisher_image = rospy.Publisher("image_out", Image)
+        # publishers
+        self.publisher_image = rospy.Publisher("image_out", Image, queue_size=1)
 
-        rate = rospy.Rate(10)
-
-        prev_time = rospy.Time.now()
-
-        while not rospy.is_shutdown():
-
-            rate.sleep()
+        rospy.spin()
 
 if __name__ == '__main__':
     try:
